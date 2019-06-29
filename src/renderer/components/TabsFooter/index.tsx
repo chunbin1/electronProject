3ee9de2c31db54ddc9f5a34e801b71333017f5fe
licb <545360100@qq.com>
2019-06-29 20:40:26 +0800

    配置tslint

--- src/renderer/components/TabsFooter/index.tsx
+++ src/renderer/components/TabsFooter/index.tsx
@@ -3,17 +3,17 @@ import { Row, Col } from 'antd';
 import IconFont from '../IconFont';
 import router from 'umi/router';
 
-export interface FooterProps {
+export interface IFooterProps {
   /** 生成底部导航 */
-  routerArr: Array<routerArr>;
+  routerArr: IRouterArr[];
 }
 
-export interface routerArr {
+export interface IRouterArr {
   icon?: string;
   route: string;
 }
 
-export default function TabsFooter(props: FooterProps) {
+export default function TabsFooter(props: IFooterProps) {
   const { routerArr } = props;
   const span: number = Math.floor(24 / routerArr.length);
   return (
